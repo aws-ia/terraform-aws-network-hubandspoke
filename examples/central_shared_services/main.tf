d1d420dee0f4687605ed91b43e4896acb6041c5c
Pablo Sanchez Carmona <pablosc@amazon.es>
2022-06-15 10:16:13 +0200

    Hub-Spoke module v1.0

---
+++ examples/central_shared_services/main.tf
@@ -0,0 +1,48 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+# --- examples/central_inspection/main.tf ---
+
+# Hub and Spoke module - we only centralize the Egress and Ingress traffic
+module "hub-and-spoke" {
+    source = "../.."
+
+    aws_region = var.aws_region
+    identifier = var.identifier
+    
+    transit_gateway = {
+        name = "tgw-${var.identifier}"
+    }
+
+    central_vpcs = {
+        shared_services = {
+            name = "shared-services-vpc"
+            cidr_block = "10.10.0.0/16"
+            az_count = 2
+
+            subnets = {
+                endpoints = {
+                    netmask = 24
+                }
+                transit_gateway = {
+                    netmask = 28
+                }
+            }
+        }
+
+        hybrid_dns = {
+            name = "hybrid-dns-vpc"
+            cidr_block = "10.20.0.0/16"
+            az_count = 2
+
+            subnets = {
+                endpoints = {
+                    netmask = 24
+                }
+                transit_gateway = {
+                    netmask = 28
+                }
+            }
+        }
+    }
+}
