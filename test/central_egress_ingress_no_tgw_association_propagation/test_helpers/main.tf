d8427d01584f5c0016bdd7129239cf6b5c90b064
Alexey Gumirov <ag_devops@die-optimisten.net>
2023-10-04 01:23:02 +0200

    Test helpers
    Module which creates assisting resources which are used by the module under test

---
+++ test/central_egress_ingress_no_tgw_association_propagation/test_helpers/main.tf
@@ -0,0 +1,19 @@
+# AWS Transit Gateway
+resource "aws_ec2_transit_gateway" "tgw" {
+
+  description                     = "Transit_Gateway-${var.identifier}"
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+  amazon_side_asn                 = 64515
+
+  tags = {
+    Name = "tgw-${var.identifier}"
+  }
+}
+
+# Managed prefix list (to pass to the Hub and Spoke module)
+resource "aws_ec2_managed_prefix_list" "network_prefix_list" {
+  name           = "Network's Prefix List"
+  address_family = "IPv4"
+  max_entries    = 2
+}
