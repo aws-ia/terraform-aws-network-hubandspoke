d41c6ffb4845c49470a02db1fd9d028d8fcaf2c7
Alexey Gumirov <ag_devops@die-optimisten.net>
2023-10-05 22:23:26 +0200

    Random region for test

--- test/central_inspection_no_tgw_association_propagation/test.sh
+++ test/central_inspection_no_tgw_association_propagation/test.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-AWS_DEFAULT_REGION="eu-central-1"
+REGIONS="eu-central-1
+eu-central-2
+eu-north-1
+eu-south-1
+eu-south-2
+eu-west-1
+eu-west-2
+eu-west-3
+us-east-1
+us-east-2
+us-west-1
+us-west-2"
+
+AWS_DEFAULT_REGION=$(echo "$REGIONS" | shuf -n 1)
 export AWS_DEFAULT_REGION
 TIMEOUT="60m"
 
